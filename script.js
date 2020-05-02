355b8f299c600b04ab25bf43b5af4d6bd8ff4245
JP Siv-Wri <64685043+jsivwri@users.noreply.github.com>
2020-05-02 13:46:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let humanScore, computerScore = 0;
-let currentRoundNumber = 1;
-
-const generateTarget = () =>{return(Math.round(9*Math.random()))};
+let humanScore=0, computerScore=0;
+let currentRoundNumber=1;
+const generateTarget=()=>{return(Math.round(9*Math.random()))};
 const compareGuesses=(player,computer,random)=>{return(Math.abs(random-computer)<Math.abs(random-player))?false:true};
 const updateScore=winner=>{winner==="human"?humanScore++:computerScore++};
 const advanceRound=()=>{currentRoundNumber++};
